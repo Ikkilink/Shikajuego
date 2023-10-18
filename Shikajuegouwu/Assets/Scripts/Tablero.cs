c89253351628050642d500edd3f72770fa142a13
Carol <ikkilink@hotmail.com>
2023-10-17 18:32:11 -0600

    Colliders, tablero y movimiento
    puse todo eso ya puedes moverte de 180 grados y 90

---
+++ Shikajuegouwu/Assets/Scripts/Tablero.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablero : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { gameObject.transform.Rotate(0, 0, 90); }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { gameObject.transform.Rotate(0, 0, -90); }
+        if (Input.GetKeyDown(KeyCode.Space)) { gameObject.transform.Rotate(0, 0, 180); }
+    }
+}
+
